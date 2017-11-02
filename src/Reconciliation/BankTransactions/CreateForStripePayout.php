cdacbc6ad2e9a6bb15c4c001ac1610500725056a
Paul Goodchild <paul@icontrolwp.com>
2017-11-02 18:50:55 +0000

    Add a sleep between creation and retrieval of manual statements

--- src/Reconciliation/BankTransactions/CreateForStripePayout.php
+++ src/Reconciliation/BankTransactions/CreateForStripePayout.php
@@ -35,6 +35,7 @@ class CreateForStripePayout {
 
 		$oBankTxn = null;
 		if ( $bSuccess ) {
+			sleep( 2 ); // to be extra sure it properly exists when we now try to find it.
 			$oBankTxn = ( new FindForStripePayout() )
 				->setConnection( $this->getConnection() )
 				->setBankAccountVo( $this->getBankAccountVo() )
